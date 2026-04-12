9334f5049550375cb4087dafc9a79635e6eff169
Ashis <ashisbhakta890@gmail.com>
2026-04-12 10:38:53 +0530

    Fix Netlify deploy by adding env var fallbacks

--- src/utils/supabase/client.js
+++ src/utils/supabase/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://wzwewxxbahntbkxafrzv.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6d2V3eHhiYWhudGJreGFmcnp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4OTYzNjMsImV4cCI6MjA5MTQ3MjM2M30.LUjtdxLgdHH-jeNePrS2ESpqOTZlGjwjcqqtpzL8YVU';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
